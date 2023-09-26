f08b64d725966b74da91b1bed551dbac3895c8de
maryam <Reemaali9048@gmail.com>
2023-09-27 01:19:05 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+const pateEmployeeWithKeyAndValue (employee, key, value) {
+    beforeEach(function () {
+    for (const key in employee) {
+      }
+      employee.name = 'Sam';
+    });
+  } 
+
+  console.log ('employee', function () {
+      (updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway')).to.eql({
+      name: 'Sam',
+      streetAddress: '11 Broadway'
+    });
+  });
